02f17077a2373061586f3acf1bdd4faa25856ecd
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2024-07-14 23:45:08 +0530

    added pipelines to get task and logs

--- server/routes/task.route.js
+++ server/routes/task.route.js
@@ -1,19 +1,20 @@
 const route=require("express").Router();
+const { default: mongoose } = require("mongoose");
 const {Task, Log}=require("../models/task.model")
 const {asyncHandler}=require("../utils/asyncHandler");
 const { getIndianTime } = require("../utils/getIndianTime");
 
-route.post("/createhabit",asyncHandler(async(req,res)=>{
-    const {name,description,userId}=req.body
+route.post("/create-task",asyncHandler(async(req,res)=>{
+    const {name,description,userId,type}=req.body
     if(!(name&&description&&userId)){
         return res.status(400).json({message:"all fields are required",success:false})
     }
-    const createdTask=new Task({name,description,userId});
+    const createdTask=new Task({name,description,userId,type});
     const task=await createdTask.save()
     res.status(200).json({task,success:true})
 }))
 
-route.get("/getallhabits",asyncHandler(async(req,res)=>{
+route.get("/get-all-tasks",asyncHandler(async(req,res)=>{
     const {userId}=req.body
     if(!userId){
         return res.status(400).json({message:"userId is required",success:false})
@@ -22,22 +23,59 @@ route.get("/getallhabits",asyncHandler(async(req,res)=>{
     return res.status(200).json({habits,success:true})
 }))
 
-route.get("/getahabit/:taskId",asyncHandler(async(req,res)=>{
+route.get("/get-task/:taskId",asyncHandler(async(req,res)=>{
     const {taskId}=req.params
-    const habit= await Task.findById(taskId)
-    let logs=await Log.find({taskId})
-    let todayLog
-    if(!(logs===undefined&&logs.length===0)){
-         todayLog= logs.filter(log=>{
-            return log.date===getIndianTime(Date.now()).toISOString().split("T")[0]
-        })
+    const habit=await Task.aggregate(
+        [
+            {
+              $match: {
+                _id: new mongoose.Types.ObjectId(taskId)
+              }
+            },
+            {
+              $lookup: {
+                from: "logs",
+                localField: "_id",
+                foreignField: "taskId",
+                as: "todayLog",
+                pipeline: [
+                  {
+                    $match: {
+                      date: getIndianTime(Date.now()).toISOString().split("T")[0],
+                    }
+                  }
+                ]
+              }
+            },
+            {
+                $addFields:{
+                      todayLog:{
+                        $first:"$todayLog"
+                    }
+                }
+            },
+            {
+              $lookup: {
+                from: "logs",
+                localField: "_id",
+                foreignField: "taskId",
+                as: "allLogs"
+              }
+            }
+          ]
+    )
+    if(habit===undefined||habit.length===0){
+        return res.status(400).json({message:"habit not found",success:false})
     }
-    return res.status(200).json({todayLog,habit,logs,success:true})
+    return res.status(200).json({habit:habit[0],success:true})
 }))
 
-route.put("/updatehabithours/:id",asyncHandler(async(req,res)=>{
+route.put("/update-task-hours/:id",asyncHandler(async(req,res)=>{
     const taskId=req.params.id
     const {hours,userId,isDone}=req.body
+    if(!(hours&&userId&&isDone)){
+        return res.status(400).json({message:"all fields are required",success:false})
+    }
     let todayLog=await Log.findOne({taskId,date:getIndianTime(Date.now()).toISOString().split("T")[0]})
     console.log(todayLog)
     if(!todayLog){
@@ -49,9 +87,80 @@ route.put("/updatehabithours/:id",asyncHandler(async(req,res)=>{
          todayLog=await todayLog.save()
     }
     return res.status(200).json({todayLog,success:true})
-    
+}))
+
+route.get("/get-all-tasks-and-logs",asyncHandler(async(req,res)=>{
+    const userId=req.body.userId
+    const habitsAndLogs= await Task.aggregate(
+        [
+            {
+              $match: {
+                userId: new mongoose.Types.ObjectId(userId)
+              }
+            },
+            {
+              $lookup: {
+                from: "logs",
+                localField: "_id",
+                foreignField: "taskId",
+                as: "logs"
+              }
+            }
+          ]
+    )
+    if(habitsAndLogs===undefined||habitsAndLogs.length===0){
+        return res.status(400).json({message:"no habits for this user",success:false})
+    }
+    return res.status(200).json({habitsAndLogs,success:true})
+}))
 
 
+route.get("/get-tasks-by-date",asyncHandler(async(req,res)=>{
+    const {date,userId}=req.body
+    console.log(date+" "+userId)
+    const tasks= await Log.aggregate(
+        [
+            {
+              $match: {
+                date: date
+              }
+            },
+            {
+              $match: {
+                userId:new mongoose.Types.ObjectId(userId)
+              }
+            },
+            {
+              $lookup: {
+                from: "tasks",
+                localField: "taskId",
+                foreignField: "_id",
+                as: "task",
+                pipeline: [
+                  {
+                    $project: {
+                      name: 1,
+                      description:1,
+                      type:1
+                      
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              $addFields: {
+                task: {
+                  $first: "$task"
+                }
+              }
+            }
+          ]
+    )
+    if(tasks===undefined||tasks.length===0){
+        return res.status(400).json({message:"no tasks in this particular date",success:false})
+    }
+    return res.status(200).json({tasks,success:true})
 }))
 
 
